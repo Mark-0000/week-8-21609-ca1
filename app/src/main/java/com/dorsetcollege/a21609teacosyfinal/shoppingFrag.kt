8cabfd3121b123591ea157de691e1057f7e37312
Mark Christian Albinto <markchristianalbinto@yahoo.com>
2020-11-12 17:49:40 +0000

    testing data binding

--- app/src/main/java/com/dorsetcollege/a21609teacosyfinal/shoppingFrag.kt
+++ app/src/main/java/com/dorsetcollege/a21609teacosyfinal/shoppingFrag.kt
@@ -18,6 +18,7 @@ class shoppingFrag : Fragment() {
     //creeating object for class shopping viewModel
     private lateinit var viewModel: shoppingViewModel
 
+    var x = 25
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -27,46 +28,59 @@ class shoppingFrag : Fragment() {
 
         view.cartbutton.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.navigate_to_cart)}
 
+
+        //button listeners to call functions
         view.adddarjelling.setOnClickListener{onD()}
         view.addassam.setOnClickListener{onA()}
         view.addlapsang.setOnClickListener{onL()}
         view.addearlgrey.setOnClickListener{onE()}
         view.addirishbreakfast.setOnClickListener{onI()}
         view.reset.setOnClickListener{onR()}
+        viewModel.delivery()
         textUpdate()
         return view
     }
+
+    //methods being called on view model
+    //add darjelling method
     private fun onD(){
         viewModel.addDarjelling()
         textUpdate()
 
     }
+    //add assam method
     private fun onA(){
         viewModel.addAssam()
         textUpdate()
 
     }
+    //add lapsang method
     private fun onL(){
         viewModel.addLapsang()
         textUpdate()
 
     }
+    //add earlgrey method
     private fun onE(){
         viewModel.addEarlGrey()
         textUpdate()
 
     }
+    //add irish breakfast method
     private fun onI(){
         viewModel.addIrishBreakfast()
         textUpdate()
 
     }
+
+    //reset method
     private fun onR(){
         textUpdate()
 
     }
+    // updating the text
     private fun textUpdate(){
-
+        //darjellingquantity.text = x.toString()
     }
 
 }
--- app/src/main/java/com/dorsetcollege/a21609teacosyfinal/shoppingViewModel.kt
+++ app/src/main/java/com/dorsetcollege/a21609teacosyfinal/shoppingViewModel.kt
@@ -3,13 +3,16 @@ package com.dorsetcollege.a21609teacosyfinal
 import androidx.lifecycle.ViewModel
 
 class shoppingViewModel: ViewModel() {
+    // declaring variables (cost and quantity)
     var totalAmount = 0.0
+    var totalQuantity = 0
 
     var darjellingcost = 8.50
     var assamcost = 7.50
     var lapsangcost = 9.50
     var earlgreycost = 3.50
     var irishbreakfastcost = 2.50
+    var deliverycost = 5.0
 
     var darjellingquan = 0
     var assamquan = 0
@@ -17,25 +20,38 @@ class shoppingViewModel: ViewModel() {
     var earlgreyquan = 0
     var irishbreakfastquan = 0
 
+    //methods being called from shoppingFrag class
+    fun delivery(){
+        if (totalQuantity < 5){
+            totalAmount += deliverycost
+
+
+        }
+    }
     fun addDarjelling(){
         darjellingquan++
         totalAmount += darjellingcost
+        totalQuantity++
     }
     fun addAssam(){
         assamquan++
         totalAmount += assamcost
+        totalQuantity++
     }
     fun addLapsang(){
         lapsangquan++
         totalAmount += lapsangcost
+        totalQuantity++
     }
     fun addEarlGrey(){
         earlgreyquan++
         totalAmount += earlgreycost
+        totalQuantity++
     }
     fun addIrishBreakfast(){
         irishbreakfastquan++
         totalAmount += irishbreakfastcost
+        totalQuantity++
     }
     fun reset(){
         totalAmount = 0.0
@@ -44,6 +60,7 @@ class shoppingViewModel: ViewModel() {
         lapsangquan = 0
         earlgreyquan = 0
         irishbreakfastquan = 0
+        totalQuantity = 0
     }
 
 }
